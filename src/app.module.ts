23d8daf3430483fe939df6d81839964f92aac658
nukambe <chappellwesley@gmail.com>
2024-05-22 17:06:18 -0400

    db url > db host

--- src/app.module.ts
+++ src/app.module.ts
@@ -16,7 +16,7 @@ import { join } from 'path';
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DATABASE_URL,
+      host: process.env.DB_HOST,
       port: parseInt(process.env.DB_PORT),
       username: process.env.DB_USER,
       password: process.env.DB_PASS,
