acd6b3a01275566aa5bf3352c15213ca1a5b3464
nukambe <chappellwesley@gmail.com>
2024-05-24 16:26:51 -0400

    fix mobile menu margins

--- client/src/features/navigation/MobileMenuButton.tsx
+++ client/src/features/navigation/MobileMenuButton.tsx
@@ -13,9 +13,9 @@ export default function MobileMenuButton({
 }) {
   return (
     <>
-      <div className="relative z-10 flex items-center lg:hidden">
+      <div className="relative z-10 flex items-center lg:hidden ml-auto">
         <Button
-          className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+          className="items-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-green-500"
           onClick={() => setOpen(!open)}
         >
           <span className="sr-only">Open menu</span>
--- client/src/features/navigation/Navigation.tsx
+++ client/src/features/navigation/Navigation.tsx
@@ -12,7 +12,7 @@ export default function Navigation() {
   return (
     <header className="bg-white shadow fixed w-full z-50">
       <div className="mx-auto max-w-7xl px-2 sm:px-4 lg:divide-y lg:divide-gray-200 lg:px-8">
-        <div className="relative flex h-16 justify-between">
+        <div className="relative flex h-16 justify-between w-full">
           <div className="relative z-10 flex px-2 lg:px-0">
             <div className="flex flex-shrink-0 items-center">
               <Logo />
@@ -21,7 +21,6 @@ export default function Navigation() {
           <Search />
           <MobileMenuButton open={open} setOpen={setOpen} />
           <div className="hidden lg:relative lg:z-10 lg:ml-4 lg:flex lg:items-center">
-            {/* Other buttons... */}
             <ProfileDropdown />
           </div>
         </div>
